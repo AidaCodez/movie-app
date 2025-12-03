0c0c1a58dd86604ab5e9392db4b0a0266998bf92
AidaCodez <211214391+AidaCodez@users.noreply.github.com>
2025-12-04 01:36:38 +0800

    feat(search): search movie using api
    create SearchBox component for searching movie title
    add MovieListHeading component on the same div
    useEffect will run on first render or when search value changes
    input searchValue will be passed to api to get new movie list

--- src/App.js
+++ src/App.js
@@ -2,26 +2,34 @@ import React, { useState , useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-  const getMovieRequest = async() => {
-    const url = "http://www.omdbapi.com/?s=avengers&apikey=1a31b66d";
+  const getMovieRequest = async(searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=1a31b66d`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
 
-    console.log(responseJson);
-    setMovies(responseJson.Search);
-  }
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
 
   useEffect(() => {
-    getMovieRequest();
-  }, []);
+    getMovieRequest(searchValue);
+  }, [searchValue]);
 
   return (
     <div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies'/>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
       <div className='row'>
         <MovieList movies={movies} />
       </div>   
